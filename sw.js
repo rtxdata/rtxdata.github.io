1f911ad625ac938f47baa87a5061ae5945de00c9
Evgenii Novikov <enovikov11@yandex.com>
2023-10-01 14:23:37 +0200

    Подготовка к релизу браузерного расширения

--- sw.js
+++ sw.js
@@ -1,8 +1,11 @@
 self.addEventListener('fetch', event => {
-    event.respondWith(
+    return event.respondWith(
         caches.open('cache').then(cache => {
             return fetch(event.request, { cache: 'no-cache' }).then(response => {
-                cache.put(event.request, response.clone());
+                if (event.request.url.startsWith('https')) {
+                    cache.put(event.request, response.clone());
+                }
+
                 return response;
             }).catch(() => {
                 return caches.match(event.request).then(response => {
