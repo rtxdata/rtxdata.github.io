a891c7205fc255e83bea9028116d4570b3b67134
Evgenii Novikov <enovikov11@yandex.com>
2023-10-05 00:54:19 +0200

    hidesum multi

--- src/HideSum.js
+++ src/HideSum.js
@@ -1,10 +1,13 @@
 import React, { useContext } from "react";
 import { Context } from './Context';
 
+let i = 0;
+
 export default function HideSum() {
     const { hideSum, setHideSum } = useContext(Context);
+    i++;
     return <div className="content">
-        <input type="checkbox" id="hideSum" value={hideSum} onChange={setHideSum} />
-        <label htmlFor="hideSum">Скрыть суммы на круговых диаграммах</label>
+        <input type="checkbox" id={"hideSum" + i} checked={hideSum} onChange={setHideSum} />
+        <label htmlFor={"hideSum" + i}>Скрыть суммы на круговых диаграммах</label>
     </div>
 }
--- src/Pie.js
+++ src/Pie.js
@@ -1,17 +1,18 @@
 import React, { useContext } from 'react';
 import Plotly from 'react-plotly.js';
 import { Context } from './Context';
+import HideSum from './HideSum';
 
 export default function Pie({ df, title }) {
     const { hideSum } = useContext(Context);
 
-    return <Plotly data={[{
-        type: 'pie',
-        labels: df.values.map(d => d[0]),
-        values: df.values.map(d => d[1]),
-        textinfo: hideSum ? 'percent' : 'value+percent'
-    }]} layout={{
-        title: title,
-        width: 700
-    }} />;
+    return <>
+        <HideSum />
+        <Plotly data={[{
+            type: 'pie',
+            labels: df.values.map(d => d[0]),
+            values: df.values.map(d => d[1]),
+            textinfo: hideSum ? 'percent' : 'value+percent'
+        }]} layout={{ title: title, width: 700 }} />
+    </>;
 }
