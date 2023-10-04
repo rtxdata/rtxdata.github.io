c71a9ad8928d17d6c6da7f5b19e13aca38254aa2
Evgenii Novikov <enovikov11@yandex.com>
2023-10-04 22:23:34 +0200

    checkpoint

--- src/Pie.js
+++ src/Pie.js
@@ -12,18 +12,3 @@ export default function Pie({ df, title }) {
         width: 700
     }} />;
 }
-
-
-window.totalPieElement = function (df, title) {
-    const div = document.createElement('div');
-
-    window.Plotly.newPlot(div, [{
-        type: 'pie',
-        labels: df.values.map(d => d[0]),
-        values: df.values.map(d => d[1]),
-        textinfo: window.pieNoSum.checked ? 'percent' : 'value+percent'
-    }], { title, width: 700 });
-
-    return div;
-}
-
